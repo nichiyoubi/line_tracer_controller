a58e4404be0a276583a483b7cb57277b098e3c86
Masanori Usami <nichiyoubinoshisya@gmail.com>
2016-06-26 22:30:59 +0900

    初期ソースコード追加

---
+++ src/main/java/jp/topse/line_tracer_controller/CommandCI.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package jp.topse.line_tracer_controller;
+
+import jp.topse.line_tracer.Command;
+
+/**
+ * @author usamimasanori
+ *
+ */
+public class CommandCI {
+	public CommandCI() {
+	}
+	
+	public void help() {
+		System.out.println("Command:");
+		System.out.println("\t 1:STRAIGHT 2:RIGHT 3:LEFT 4:STOP OTHERS:ERROR");
+	}
+	
+	public String parse(String input) {
+		try {
+			int no = Integer.parseInt(input);
+			switch(no) {
+			case 1:
+				return Command.STRAIGHT_;
+			case 2:
+				return Command.RIGHT_;
+			case 3:
+				return Command.LEFT_;
+			case 4:
+				return Command.STOP_;
+			default:
+				return Command.ERROR_;
+			}
+		}
+		catch(NumberFormatException e) {
+			return Command.ERROR_;
+		}
+	}
+}
---
+++ src/main/java/jp/topse/line_tracer_controller/PCLineTracer.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package jp.topse.line_tracer_controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @author usamimasanori
+ *
+ */
+public class PCLineTracer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Socket socket_  = null;
+		final int PORT_ = 12345;
+		
+		try {
+			socket_ = new Socket(args[0], PORT_);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket_.getInputStream()));
+			PrintWriter out  = new PrintWriter(socket_.getOutputStream());
+			BufferedReader keyin = new BufferedReader(new InputStreamReader(System.in));
+			
+			recv();
+			
+			String input;
+			CommandCI ci = new CommandCI();
+			ci.help();
+			while((input = keyin.readLine()).length() > 0) {
+				out.println(ci.parse(input));
+				out.flush();
+				
+				String line = in.readLine();
+				System.out.println("recv:" + line);
+				if (line != null) {
+					System.out.println(line);
+				}
+				else {
+					break;
+				}
+				ci.help();
+			}
+		}
+		catch(UnknownHostException e1) {
+
+		}
+		catch(IOException e2) {
+			
+		}
+	}
+	
+	/*
+	 * 
+	 */
+	public static void recv() {
+		Thread thread = new Thread(new DataReceiver());
+		thread.start();
+	}
+}
